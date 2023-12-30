0ef8f7b419cb59c4c01217bdc5f5257ae29fd1e3
Abhishek <abhisheksinghasr734@gmail.com>
2023-12-30 10:34:30 +0530

    asyncFilter

---
+++ 07_AsyncFilter.js
@@ -0,0 +1,59 @@
+/*
+- Implement a function that takes an array of input and an async
+iteratee function
+
+- returns a promise that resolves with the list of
+inputs that has passed the test through the iteratee function
+
+
+- The asynchronous iteratee function will accept an input and a
+callback. The callback function will be called when the input is
+finished processing, the first argument of the callback will be the error
+flag and the second will be the result.
+
+
+*/
+const asyncFilter = async (arr, fn) => {
+  let output = [];
+  for (let current of arr) {
+    try {
+      const result = await new Promise((resolve, reject) => {
+        fn(current, (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+      if (result) {
+        output.push(result);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  return output;
+};
+
+const numPromise = asyncFilter([1, 2, 2, 3, 4, 5], (num, callback) => {
+  setTimeout(() => {
+    num = num * 2;
+    console.log(num);
+
+    if (num === 4) {
+      //break condition
+      callback(true);
+    } else {
+      callback(null, num !== 8);
+    }
+  }, 2000);
+});
+
+numPromise
+  .then((result) => {
+    console.log("Result is " + result.join(","));
+  })
+  .catch(() => {
+    console.log("No success");
+  });
